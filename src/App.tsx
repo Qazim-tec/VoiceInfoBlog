bf7d09fc6e091c0a3f5c3a50c34239bba74487b1
DAUDU QAZIM <yetkemsupper@gmail.com>
2025-04-04 16:18:42 +0100

    scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom"; // Add useLocation
+import { useEffect } from "react"; // Add useEffect
 import Navbar from "./components/Navbar/Navbar";
 import FeaturedPostsCarousel from "./components/FeaturedPost/FeaturedPostsCarousel";
 import PostDetail from "./components/PostDetail/PostDetail";
@@ -13,11 +14,18 @@ import TrendingPosts from "./components/TrendingPosts/TrendingPosts";
 import Footer from "./components/Footer/Footer";
 import CategoriesSection from "./components/CategoriesSection/CategoriesSection";
 import MyPosts from "./components/MyPosts/MyPosts";
-import ProfilePage from "./components/ProfilePage/ProfilePage"; // Add this import
+import ProfilePage from "./components/ProfilePage/ProfilePage";
 
 import "./App.css";
 
 const App: React.FC = () => {
+  const location = useLocation(); // Hook to track route changes
+
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]); // Trigger when the pathname changes
+
   return (
     <div className="App">
       <Navbar />
@@ -27,7 +35,6 @@ const App: React.FC = () => {
           element={
             <main>
               <h1>Unveil the Stories That Matter</h1>
-              
               <FeaturedPostsCarousel />
               <LatestNews />
               <CategoriesSection />
@@ -40,7 +47,7 @@ const App: React.FC = () => {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/create-post" element={<CreatePost />} />
         <Route path="/my-posts" element={<MyPosts />} />
-        <Route path="/profile" element={<ProfilePage />} /> {/* Add this route */}
+        <Route path="/profile" element={<ProfilePage />} />
         <Route path="/:categoryName" element={<CategoryPage />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/admin/feature-posts" element={<FeaturePosts />} />
